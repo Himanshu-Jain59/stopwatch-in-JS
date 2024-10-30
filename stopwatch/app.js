93a2720fbe84b076e1f9a8720abaa1acf836d94a
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2024-10-30 20:08:04 +0530

    code updated

--- stopwatch/app.js
+++ stopwatch/app.js
@@ -1,32 +1,36 @@
 const display = document.getElementById("display");
+const start = document.getElementById("startBtn");
+const stop = document.getElementById("stopBtn");
+const reset = document.getElementById("resetBtn");
+
 let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
 
-function start() {
+start.addEventListener("click", () => {
   if (!isRunning) {
     startTime = Date.now() - elapsedTime;
     timer = setInterval(update, 10);
     isRunning = true;
   }
-}
+});
 
-function stop() {
+stop.addEventListener("click", () => {
   if (isRunning) {
     clearInterval(timer);
     elapsedTime = Date.now() - startTime;
     isRunning = false;
   }
-}
+});
 
-function reset() {
+reset.addEventListener("click", () => {
   clearInterval(timer);
   startTime = 0;
   elapsedTime = 0;
   isRunning = false;
   display.textContent = "00:00:00:00";
-}
+});
 
 function update() {
   const currentTime = Date.now();
